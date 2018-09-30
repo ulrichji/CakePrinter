09442ddd086e4a48b8202d4ce6761837f60655c2
Ulrich Johan Isachsen <ulrichji@stud.ntnu.no>
2018-09-30 13:02:37 +0200

    Added a file binarizer

---
+++ python/imgproc/image_canny_binarizer.py
@@ -0,0 +1,34 @@
+import image_provider
+
+import image_file_loader
+import image_scaler
+import image_simple_grayscaler
+
+import cv2
+
+class ImageCannyBinarizer(image_provider.ImageProvider):
+	def __init__(self, image_provider, min_threshold=100, max_threshold=200, aperture_size=3, l2gradient=False):
+		self.image_provider = image_provider
+		self.min_threshold = min_threshold
+		self.max_threshold = max_threshold
+		self.aperture_size = aperture_size
+		self.l2gradient = l2gradient
+	
+	def getImage(self):
+		return cv2.Canny(
+			image=self.image_provider.getImage(),
+			threshold1=self.min_threshold,
+			threshold2=self.max_threshold,
+			apertureSize=self.aperture_size,
+			L2gradient=self.l2gradient)
+
+def main():
+	file_image = image_file_loader.ImageFileLoader("examples/obama.jpg")
+	scale_image = image_scaler.ImageScaler(file_image, (4000,4000))
+	gray_image = image_simple_grayscaler.ImageSimpleGrayscaler(scale_image)
+	canny_image = ImageCannyBinarizer(gray_image, 100, 125, 3)
+	binarized_image = canny_image.getImage()
+	cv2.imwrite("examples/obama_canny.jpg", binarized_image)
+
+if __name__ == "__main__":
+	main()
